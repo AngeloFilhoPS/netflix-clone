513f991a0328809b29cbcf5535a838744f501346
Angelo <ampsf@icomp.ufam.edu.br>
2020-11-02 12:08:59 -0400

    feat: featured movie complete and header begin

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,16 +1,39 @@
 import React from "react";
 import './FeaturedMovie.css'
 
-export default ({item})=>{
-    return(
-        <section className='featured' style={{
-            background:'cover',
-            backgroundPosition:'center',
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-        }}>
+export default ({ item }) => {
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name)
+
+    }
+
+
+
+    return (
+        <section className='featured'
+            style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+            }}>
             <div className='featured--vertical'>
                 <div className='featured--horizontal'>
-                    
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--info'>
+                        <div className='featured--points'>{item.vote_average} pontos</div>
+                        <div className='featured--year'>{firstDate.getFullYear()}</div>
+                        <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} </div>
+                        <div className='featured--description'>{item.overview}</div>
+                        <div className='featured--buttons'>
+                            <a href='' className='featured--watchbutton'>▶ Assistir</a>
+                            <a href='' className='featured--mylistbutton'><strong>+</strong> Minha lista</a>
+                        </div>
+                        <div className='featured--genres'><strong>Gêneros:</strong> {genres.join(', ')}</div>
+
+                    </div>
+
                 </div>
             </div>
 
